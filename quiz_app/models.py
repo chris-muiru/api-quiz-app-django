3d440505aa59e096e8cf331ab6869f958cb24eee
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-07 02:06:18 +0300

    add: add a QuizModel database model

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Quiz(models.Model):
+class QuizModel(models.Model):
     userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.CharField(max_lenght=255)
-    answer = models.CharField(max_lenght=255)
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
     choices = models.JSONField()
     discussion = models.TextField(),
+
+    def __str__(self):
+        return self.question
+
 # Create your models here.
