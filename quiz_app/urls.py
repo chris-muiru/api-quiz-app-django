46f772eb9efa4ee0f401d94ff1b1196e0c1cd53e
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-08 17:09:12 +0300

    add: url to list,create,update and delete querysets

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-# from .views import  
-urlpatterns =[
-    # path('')
+from .views import QuizListView, QuizDetailsView
+
+urlpatterns = [
+    path('', QuizListView.as_view(), name="QuizListView"),
+    path('<int:pk>/', QuizDetailsView.as_view(), name="QuizListById")
 ]
