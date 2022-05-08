172612153d60874031fe316db8741137234e016c
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-08 17:07:45 +0300

    add: add crud functionalities  for QuizListView

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,11 +1,42 @@
 from rest_framework.views import APIView
-from rest_framework import status,request
+from rest_framework import status, request
 from rest_framework.response import Response
+from .serializers import QuizSerializer
+from .models import QuizModel
+
 
 class QuizListView(APIView):
-    def get(self,request):
+    def get(self, request):
+        query = QuizModel.objects.all()
+        serializer = QuizSerializer(query, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = QuizSerializer(data=request.data)
+        if serializer.is_valid():
+            # todo: save serializer with the current authenticated person
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# crud functionality
+
+
+class QuizDetailsView(APIView):
+    def get(self, request, pk):
+        query = QuizModel.objects.get(id=pk)
+        serializer = QuizSerializer(query)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        query = QuizModel.objects.get(id=pk)
+        serializer = QuizSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        quiz = QuizModel.objects.get(id=pk).delete()
         return Response(status=status.HTTP_200_OK)
-
-
-
 # Create your views here.
