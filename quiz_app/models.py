0550226f08a5bfb3b04181e470137c17bd13eb1c
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-09 15:22:15 +0300

    add: add choices field

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 
 
 class QuizModel(models.Model):
-    user_pk = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.CharField(max_length=255)
-    answer = models.JSONField()
+    answer = models.CharField(max_length=255)
+    choices = models.JSONField()
     discussion = models.CharField(max_length=255)
 
     def __str__(self):
