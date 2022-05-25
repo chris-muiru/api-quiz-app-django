94e397ce104d7cba0dd162063dee01023629bd8c
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-25 09:42:18 +0300

    add: post request to return answer status

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import status, request
 from rest_framework.response import Response
 from .serializers import QuizSerializer
-from .models import QuizModel
+from .models import QuizModel, ScoreModel
 
 
 class QuizListView(APIView):
@@ -27,6 +27,22 @@ class QuizDetailsView(APIView):
         query = QuizModel.objects.get(id=pk)
         serializer = QuizSerializer(query)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk):
+        query = QuizModel.objects.get(id=pk)
+        answer = request.data['answer']
+        if query.answer == str(answer):
+            if not ScoreModel.objects.filter(user__username=request.user).exists():
+                print('ok')
+                ScoreModel(user=request.user, total=1).save()
+
+            else:
+                instance = ScoreModel.objects.get(user__username=request.user)
+                instance.total = instance.total+1
+                instance.save()
+            return Response({"status": "correct"})
+        else:
+            return Response({"status": "incorrect"})
 
     def put(self, request, pk):
         query = QuizModel.objects.get(id=pk)
