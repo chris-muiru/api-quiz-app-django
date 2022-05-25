2e305103772787488c97412e8fc6846ad942d2b2
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-25 09:39:51 +0300

    register: register Score Model

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import QuizModel
+from .models import QuizModel, ScoreModel
 
 admin.site.register(QuizModel)
+admin.site.register(ScoreModel)
 # Register your models here.
