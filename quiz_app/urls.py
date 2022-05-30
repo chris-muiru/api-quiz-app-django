9def886779751c97691a069e2ff0a640d1858642
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-05-30 03:08:42 +0300

    add: add score url pattern

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import QuizListView, QuizDetailsView
-
+from .views import QuizListView, QuizDetailsView,ScoreView
 urlpatterns = [
     path('', QuizListView.as_view(), name="QuizListView"),
-    path('<int:pk>/', QuizDetailsView.as_view(), name="QuizListById")
+    path('<int:pk>/', QuizDetailsView.as_view(), name="QuizListById"),
+    path('score/',ScoreView.as_view(), name="Score")
 ]
